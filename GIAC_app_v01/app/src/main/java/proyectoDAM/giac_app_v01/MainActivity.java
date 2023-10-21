ab0102c17cf22475fcf316efcd68d7669574209f
A.Martos <alberto.martos.jabonero@gmail.com>
2023-10-21 10:12:39 +0200

    PRIMER COMMIT. Proyecto vacio

---
+++ GIAC_app_v01/app/src/main/java/proyectoDAM/giac_app_v01/MainActivity.java
@@ -0,0 +1,14 @@
+package proyectoDAM.giac_app_v01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
